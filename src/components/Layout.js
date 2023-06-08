931200434623bd5a50afba1a61e485528912e080
Romeran Rodriguez <romeran.rodriguez@bitnat.net>
2023-06-08 11:34:49 -0400

    previo a montarse a vercel

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,23 @@
 import { Outlet } from "react-router-dom"
 
 const Layout = () => {
-    return <Outlet />
+    return
+    <>
+          <button onClick={()=> {
+    fetch('http://192.168.3.239:8000/resultado/data', {
+  //    credentials:'include',
+      headers: {
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXV0aC9sb2dpbiIsImlhdCI6MTY4NjA2NjI4MiwiZXhwIjoxNjg2MDY5ODgyLCJuYmYiOjE2ODYwNjYyODIsImp0aSI6Imh1d2xhbzQ3aUdWdTdhamEiLCJzdWIiOiIxIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.JGEpnejhXMzYGM7ix6lnigW5YLhPZ9snC7UYsQm0v2c'
+      }
+      
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+  }}>Haz clic aquí para obtener los datos</button>
+   <Outlet />
+    </>
+   
 }
 
 export default Layout
